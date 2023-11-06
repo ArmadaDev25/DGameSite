6bce4f1da49aa75a9ff4b59c3a6d83f6360bb463
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-11-06 15:08:07 -0700

    Worked on URLS and created/registered the models

--- gamesite_project/GS_Backend/models.py
+++ gamesite_project/GS_Backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    gamename = models.CharField(max_length=120)
+
+class LeaderboardEntry(models.Model):
+    name = models.CharField(max_lenght=120)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.name
