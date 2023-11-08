12e4b79e3ecc4786d213c84f470d7d98cac801a3
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-11-07 20:59:30 -0700

    Database setup

--- gamesite_project/GS_Backend/views.py
+++ gamesite_project/GS_Backend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LBEntrySerializer
+from .models import LeaderboardEntry
 
 # Create your views here.
+
+# Backend Api Views
+class LBEntryView(viewsets.ModelViewSet):
+    serializer_class = LBEntrySerializer
+    queryset = LeaderboardEntry.objects.all()
+
+
--- gamesite_project/gamesite_project/urls.py
+++ gamesite_project/gamesite_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('GS_Backend.urls')),
+    #path('api/', include('GS_Backend.urls')),
     path('admin/', admin.site.urls),
 ]
