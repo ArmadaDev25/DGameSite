b08359aaac53cdad85cef34e898c8de3229544f6
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-11-08 18:36:56 -0700

    added an about page and registered the game model

--- gamesite_project/GS_Backend/admin.py
+++ gamesite_project/GS_Backend/admin.py
@@ -3,3 +3,4 @@ from .models import Game, LeaderboardEntry
 
 # Register your models here.
 admin.site.register(LeaderboardEntry)
+admin.site.register(Game)
--- gamesite_project/GS_Frontend/views.py
+++ gamesite_project/GS_Frontend/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
