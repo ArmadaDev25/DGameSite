bc04e480b814526b807c6e987a8e1a5c802f9b08
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-11-11 17:31:55 -0700

    Started working on the game card views in the game_list page

--- gamesite_project/GS_Backend/models.py
+++ gamesite_project/GS_Backend/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
     name = models.CharField(max_length=120)
+    developer = models.CharField(max_length=120)
+    
 
     def __str__(self):
         return self.name
