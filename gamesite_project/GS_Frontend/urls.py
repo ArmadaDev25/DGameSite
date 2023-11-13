bfc184d9520efb13150f0ec70fe5092267d25e5e
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-11-12 21:12:31 -0700

    Added footer buttons and functionality to the Game Card element on the game_list template. Worked on the game details page

--- gamesite_project/GS_Frontend/urls.py
+++ gamesite_project/GS_Frontend/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('games/', views.GameList.as_view(template_name="game_list.html"), name='game_list'),
-    path('games/<int:pk>/', views.GameDetail.as_view(template_name="game_pages/game_detail"), name='game_deatil')
+    path('games/<int:pk>/', views.GameDetail.as_view(template_name="game_pages/game_details.html"), name='game_detail')
 ]
